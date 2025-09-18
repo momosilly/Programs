c4e21f08fb4e95e6f45b79ffd5dda53feccd007d
Mohamad <mohamad.mosilly@outlook.com>
2025-09-18 11:40:51 +0200

    Created weak and moderate

--- Python/Password strength checker.py
+++ Python/Password strength checker.py
@@ -3,9 +3,13 @@ print("This application will tell you the strength of your password based on a s
 user_password = input("Enter your password: ")
 
 import re
-very_weak = r"^[a-z]+$|^[A-Z]+$|^[0-9]{1,6}$|^.{1,5}$" #Explanation: ^[a-z]+$ only lowercase letters any lenght, ^[A-Z]+$ same as lowercase condition ^[0-9]{1,6}$ only digits up to 6 characters ^.{1,5}% any combination but < 5 total
+very_weak = r"^[a-z]+$|^[A-Z]+$|^[0-9]{1,6}$|^.{1,5}$" #Explanation: ^[a-z]+$ only lowercase letters any lenght, ^[A-Z]+$ same as lowercase condition ^[0-9]{1,6}$ only digits up to 6 characters ^.{1,5}% any combination but =< 5 total
+weak = r"^[a-z0-9]{5,8}$"
+moderate = r"^[a-zA-z0-9]{8,10}$"
 
 if re.fullmatch(very_weak, user_password):
     print("Very weak")
-else:
-    print("Looks strong")
+elif re.fullmatch(weak, user_password):
+    print("Weak")
+elif re.fullmatch(moderate, user_password):
+    print("Moderate")
