5161c7b53803dbfb89c1ee52ecd76b3eceb37e4f
Mohamad <mohamad.mosilly@outlook.com>
2025-09-18 11:14:26 +0200

    Fixed very_weak regex

--- Python/Password strength checker.py
+++ Python/Password strength checker.py
@@ -3,7 +3,7 @@ print("This application will tell you the strength of your password based on a s
 user_password = input("Enter your password: ")
 
 import re
-very_weak = r"^[0-9]$|^[a-z]{1,6}$|^[A-Z]{1,6}$|^.{1,5}$"
+very_weak = r"^[a-z]+$|^[A-Z]+$|^[0-9]{1,6}$|^.{1,5}$" #Explanation: ^[a-z]+$ only lowercase letters any lenght, ^[A-Z]+$ same as lowercase condition ^[0-9]{1,6}$ only digits up to 6 characters ^.{1,5}% any combination but < 5 total
 
 if re.fullmatch(very_weak, user_password):
     print("Very weak")
