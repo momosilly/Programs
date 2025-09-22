996d4feb703c2f1fa2333c7308097d10792a1ff1
Mohamad <mohamad.mosilly@outlook.com>
2025-09-22 14:19:57 +0200

    Built Query and pushed to github

--- Python/movie-suggester/Movie.py
+++ Python/movie-suggester/Movie.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("TMDB_API_KEY")
 
-#mood = input("What's your mood? (e.g., happy, sad, adventurous): ").lower()
-#language = input("Preferred language? (e.g., en, nl, fr): ").lower()
-#actor_name = input("Favorite actor? (optional): ").strip()
-#year = input("Preferred release year? (optional): ").strip()
+mood = input("What's your mood? (e.g., happy, sad, adventurous): ").lower()
+language = input("Preferred language? (e.g., en, nl, fr): ").lower()
+actor_name = input("Favorite actor? (optional): ").strip()
+year = input("Preferred release year? (optional): ").strip()
+min_rating = input("Preferred minimum rating: ")
 
 mood_to_genre = {
     "happy": ["comedy", "romance", "animation", "music"],
@@ -44,12 +45,40 @@ def get_actor_id(name):
     response = requests.get(url, params=params)
     results = response.json().get("results", [])
     return results[0]["id"] if results else None
-#actor_name = input("Enter an actor's name to test: ").strip()
-#actor_id = get_actor_id(actor_name)
 
-#if actor_id:
-    #print(f"Actor ID for '{actor_name}': {actor_id}")
-#else:
-#    print("Actor not found")
 def format_language_code(code):
     return f"{code}-US" if code else "en-US"
+
+def build_query(mood, language, actor_name, min_rating):
+    genre_ids_map = get_genre_ids()
+    genres = mood_to_genre.get(mood, [])
+    genre_ids = [str(genre_ids_map[g]) for g in genres if g in genre_ids_map] #!
+
+    actor_id = get_actor_id(actor_name)
+    lang_code = format_language_code(language)
+
+    params = {
+        "api_key": API_KEY,
+        "with_genres": ",".join(genre_ids),
+        "language": lang_code,
+        "sort_by": "popularity.desc",
+        "page": 1
+    }
+
+    if actor_id:
+        params["with_cast"] = actor_id
+    if min_rating:
+        params["vote_average.gte"] = min_rating
+    
+    return params
+
+def get_movies(params):
+    url = "https://api.themoviedb.org/3/discover/movie"
+    response = requests.get(url, params=params)
+    results = response.json().get("results", [])
+    for movie in results[:5]:
+        print(f"{movie['title']} ({movie['release_date']})")
+        print(f"Overview: {movie['overview']}\n")
+
+params = build_query(mood, language, actor_name, min_rating)
+get_movies(params)
