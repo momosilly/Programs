01ce80a5cbb411f24eb10959c160d80b7dc2dead
Mohamad <97125106@st.deltion.nl>
2026-03-19 12:12:52 +0100

    Improved styling on both login and signup screens

--- React Native/SprintPlanner/app/(modals)/login.tsx
+++ React Native/SprintPlanner/app/(modals)/login.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { View, TextInput, Button, Text, Alert } from 'react-native'
+import { View, TextInput, Button, Text, Alert, StyleSheet, Pressable, Platform } from 'react-native'
 import { useRouter } from "expo-router";
 import { login } from "../../src/api";
 import { getUserFromToken } from "../../src/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getAuthKey } from "../../src/storage/keys";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginModal() {
     const router = useRouter();
@@ -27,25 +28,94 @@ export default function LoginModal() {
     };
 
     return (
-        <View style={{ padding: 20 }}>
-            <Text style={{ fontSize: 22, fontWeight: "bold" }}>Login</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={{ fontSize: 25, fontWeight: "bold", alignSelf: 'center', marginBottom: 20 }}>Login</Text>
 
             <TextInput 
                 placeholder="Email"
                 value={email}
                 onChangeText={setEmail}
-                style={{ marginVertical: 10 }}
+                style={styles.input}
             />
             <TextInput 
                 placeholder="Password"
                 secureTextEntry
                 value={password}
                 onChangeText={setPassword}
-                style={{ marginVertical: 10 }}
+                style={styles.input}
             />
 
-            <Button title="Login" onPress={handleLogin} />
-            <Button title="Don't have an account? Signup" onPress={() => router.push("/(modals)/signup")} />
-        </View>
+            <Pressable 
+                onPress={handleLogin} 
+                style={({pressed}) => [styles.pressable, pressed && styles.pressablePressed]}
+            >
+                <Text style={styles.pressableText}>Login</Text>
+            </Pressable>
+            <Pressable 
+                onPress={() => router.push("/(modals)/signup")}
+            >
+                <Text style={{ fontSize: 18, textAlign: 'center', marginVertical: 20 }}>Don't have an account? <Text style={{color: 'blue'}}>Sign up</Text></Text>
+            </Pressable>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 30,
+        marginVertical: '60%'
+    },
+    input: {
+        paddingVertical: Platform.OS === "android" ? 12 : 10,
+        paddingHorizontal: 14,
+        marginVertical: 10,
+        backgroundColor: "#fff",
+        fontSize: Platform.OS === "android" ? 16 : 17,
+
+        ...(Platform.OS === "android"
+        ? {
+            borderRadius: 6,
+            borderWidth: 1,
+            borderColor: "#ccc",
+            elevation: 1,
+            }
+        : {
+            borderRadius: 10,
+            borderWidth: StyleSheet.hairlineWidth,
+            borderColor: "#ccc",
+            }),
+
+    },
+    pressable: {
+        marginTop: 20,
+        paddingTop: 3,
+        paddingBottom: 3,
+        alignItems: 'center',
+        alignSelf: 'center',
+        borderColor: '#1b6cef',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: '#1b6cef',
+        width: 240,
+
+        ...(Platform.OS === 'android'
+            ? {
+                elevation: 2
+            }
+            : {
+                borderWidth: StyleSheet.hairlineWidth
+            }
+        ) 
+    },
+    pressableText: {
+        color: "#fff",
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    pressablePressed: {
+        opacity: Platform.OS === 'ios' ? 0.5 : 1,
+        transform: Platform.OS === 'android' ? [{ scale: 0.97 }] : undefined
+    },
+})
--- React Native/SprintPlanner/app/(modals)/signup.tsx
+++ React Native/SprintPlanner/app/(modals)/signup.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { View, Text, TextInput, Button, BackHandler, Alert } from 'react-native'
+import { View, Text, TextInput, Button, BackHandler, Alert, StyleSheet, Platform, Pressable } from 'react-native'
 import { useRouter } from "expo-router";
 import { signup } from "../../src/api";
 import { getUserFromToken } from "../../src/auth";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SignupModal() {
     const router = useRouter();
@@ -36,30 +37,100 @@ export default function SignupModal() {
     } ,[])
 
     return (
-        <View style={{ padding: 20 }}>
-            <Text style={{ fontSize: 22, fontWeight: "bold" }}>Signup</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={{ fontSize: 25, fontWeight: "bold", alignSelf: 'center', marginBottom: 20 }}>Signup</Text>
 
             <TextInput 
                 placeholder="Name"
                 value={name}
                 onChangeText={setName}
-                style={{ marginVertical: 10 }}
+                style={styles.input}
             />
             <TextInput 
                 placeholder="Email"
                 value={email}
                 onChangeText={setEmail}
-                style={{ marginVertical: 10 }}
+                style={styles.input}
             />
             <TextInput 
                 placeholder="Password"
+                secureTextEntry
                 value={password}
                 onChangeText={setPassword}
-                style={{ marginVertical: 10 }}
+                style={styles.input}
             />
 
-            <Button title="Signup" onPress={handleSignup} />
-            <Button title="Already have an account? Login" onPress={() => router.push("/(modals)/login")} />
-        </View>
+            <Pressable 
+                onPress={handleSignup}
+                style={styles.pressable}
+            >
+                <Text style={styles.pressableText}>Sign up</Text>
+            </Pressable>
+            <Pressable
+                onPress={() => router.push("/(modals)/login")}
+            >
+                <Text style={{ fontSize: 18, textAlign: 'center', marginVertical: 20 }}>Already have an account? <Text style={{ color: 'blue' }}>Login</Text></Text>
+            </Pressable>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 30,
+        marginVertical: '60%'
+    },
+    input: {
+        paddingVertical: Platform.OS === "android" ? 12 : 10,
+        paddingHorizontal: 14,
+        marginVertical: 10,
+        backgroundColor: "#fff",
+        fontSize: Platform.OS === "android" ? 16 : 17,
+
+        ...(Platform.OS === "android"
+        ? {
+            borderRadius: 6,
+            borderWidth: 1,
+            borderColor: "#ccc",
+            elevation: 1,
+            }
+        : {
+            borderRadius: 10,
+            borderWidth: StyleSheet.hairlineWidth,
+            borderColor: "#ccc",
+            }),
+
+    },
+    pressable: {
+        marginTop: 20,
+        paddingTop: 3,
+        paddingBottom: 3,
+        alignItems: 'center',
+        alignSelf: 'center',
+        borderColor: '#1b6cef',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: '#1b6cef',
+        width: 240,
+
+        ...(Platform.OS === 'android'
+            ? {
+                elevation: 2
+            }
+            : {
+                borderWidth: StyleSheet.hairlineWidth
+            }
+        ) 
+    },
+    pressableText: {
+        color: "#fff",
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    pressablePressed: {
+        opacity: Platform.OS === 'ios' ? 0.5 : 1,
+        transform: Platform.OS === 'android' ? [{ scale: 0.97 }] : undefined
+    },
+})
